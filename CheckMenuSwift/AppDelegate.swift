f999522f632ad9829f3ba4e778edc731637e17a1
Illia Akhaiev <twealm@marvin.local>
2015-06-18 18:26:49 +0300

    Initial Commit

---
+++ CheckMenuSwift/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CheckMenuSwift
+//
+//  Created by Illia Akhaiev on 6/18/15.
+//  Copyright (c) 2015 Illia Akhaiev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
